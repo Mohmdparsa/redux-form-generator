d91264b4f39a6abee6b7c0132cbd47fe62634340
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-02-21 09:11:59 -0800

    feat(main.jsx): implement store in main.jsx

--- redux-form/src/main.jsx
+++ redux-form/src/main.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import store from "./app/store.js";
+import { Porvider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App/>}>
-          
-        </Route>
-      </Routes>
+      <Porvider store={store}>
+        <Routes>
+          <Route path="/" element={<App />}></Route>
+        </Routes>
+      </Porvider>
     </BrowserRouter>
   </React.StrictMode>
 );
