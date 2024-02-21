d5eee4e768d9c7e531dc4431ba29abc17d6b78f9
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-02-21 12:32:32 -0800

    chore(main.jsx): import WelcomePage

--- redux-form/src/main.jsx
+++ redux-form/src/main.jsx
@@ -3,16 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import store from "./app/store.js";
-import { Porvider } from "react-redux";
+import { Provider } from "react-redux";
+import WelcomePage from "./components/WelcomePage.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Porvider store={store}>
+    {" "}
+    <Provider store={store}>
+      <BrowserRouter>
         <Routes>
-          <Route path="/" element={<App />}></Route>
+          <Route path="/" element={<App />} />
+          <Route path="/WelcomePage" element={<WelcomePage />} />
         </Routes>
-      </Porvider>
-    </BrowserRouter>
+      </BrowserRouter>{" "}
+    </Provider>
   </React.StrictMode>
 );
