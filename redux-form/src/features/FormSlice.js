c1de9c55b3e82faa7738d169f05e29697b176363
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-02-21 09:14:05 -0800

    feat(FormSlice.js): create FormSlice component for reducer

---
+++ redux-form/src/features/FormSlice.js
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const FormSlice = createSlice({
+    name:"form",
+    initialState:{name:""},
+    reducers:{}
+})
+
+export default FormSlice.reducer
